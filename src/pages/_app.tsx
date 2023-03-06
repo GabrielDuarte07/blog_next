a28f36c48b260dff691a4980fc195dc88468c245
gabriel <gabriel.gd7@outlook.com>
2023-03-06 17:51:08 -0300

    styled-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+//import "@/styles/globals.css";
+import { GlobalStyles } from "@/styles/global_styles";
+import { theme } from "@/styles/theme";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+// const Container = styled.div`
+//   background-color: ${({ theme }) => theme.colors.primary};
+// `;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
